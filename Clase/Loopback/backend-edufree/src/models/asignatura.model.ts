0a7f3efc2c9b626fbe8ad3c7954cedc77f1247f6
Lorena Benítez <78233707+LorenaBtz@users.noreply.github.com>
2021-11-06 19:57:18 -0500

    Clase 6Nov
    Creacion del Modelo, Repositorio y Controlador de:
    - Programas Academicos
    - Asignaturas

---
+++ Clase/Loopback/backend-edufree/src/models/asignatura.model.ts
@@ -0,0 +1,43 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {ProgramaAcademico} from './programa-academico.model';
+
+@model()
+export class Asignatura extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaCreacion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  profundizacion: string;
+
+  @belongsTo(() => ProgramaAcademico)
+  programaAcademicoId: string;
+
+  constructor(data?: Partial<Asignatura>) {
+    super(data);
+  }
+}
+
+export interface AsignaturaRelations {
+  // describe navigational properties here
+}
+
+export type AsignaturaWithRelations = Asignatura & AsignaturaRelations;
