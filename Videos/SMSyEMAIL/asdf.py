d23a980fb298762f150035bd43421919d8c29ce1
Lorena Benítez <78233707+LorenaBtz@users.noreply.github.com>
2021-11-09 19:02:33 -0500

    Videos SMS EMail

---
+++ Videos/SMSyEMAIL/asdf.py
@@ -0,0 +1,12 @@
+"""
+Created on Tue Nov  9 17:04:32 2021
+@author: lyda_
+"""
+
+import os
+
+os.environ["TWILIO_ACCOUNT_SID"]="AC5b285b4d9b65856a50dd41b0c527d97a"
+os.environ["TWILIO_AUTH_TOKEN"]="a8f0e0eb1e18cd3ed986ffe181939b5a"
+os.environ["SENDGRID_API_KEY"]="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"
+os.environ["FROM_EMAIL"]="lorena.btz.dev@gmail.com"
+os.environ["Test"]="Prueba de Variable de Entorno"
