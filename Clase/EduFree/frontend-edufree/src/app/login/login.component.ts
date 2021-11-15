98a6448fdf7405bc4bdbcf530d1802ae1668e2fb
Lorena Benítez <78233707+LorenaBtz@users.noreply.github.com>
2021-11-15 18:09:47 -0500

    Angular 14 Nov Parte 2

--- Clase/EduFree/frontend-edufree/src/app/login/login.component.ts
+++ Clase/EduFree/frontend-edufree/src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+interface User {
+  usuario: string;
+  contrasenia: string;
+}
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  titulo = 'Login EduFree';
+  loginUser : User = {usuario:'',contrasenia:''};
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostrarInput(): void {
+    alert("Nombre de Ususario: " + this.loginUser.usuario);
+  }
 }
